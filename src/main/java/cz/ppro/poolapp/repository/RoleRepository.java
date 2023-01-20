052e94a5af5bdd57fc497fcbc9e7607d0cfb7065
Ovik10 <svobodajindrich@centrum.cz>
2023-01-20 19:45:38 +0100

    Adding Role

---
+++ src/main/java/cz/ppro/poolapp/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package cz.ppro.poolapp.repository;
+
+import cz.ppro.poolapp.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role,Long> {
+    Role findByName(String name);
+}
