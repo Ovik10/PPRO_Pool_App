99431451a9b9999c430be472a1a6d1c1125013cc
Ovik10 <70468075+Ovik10@users.noreply.github.com>
2023-01-21 09:56:29 +0100

    Update Account.js

--- poolappfe/src/components/Account.js
+++ poolappfe/src/components/Account.js
@@ -1,15 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Container} from '@mui/system';
-import Button from '@mui/material-next/Button';
 
 export default function Account() {
+  
   const[login, setLogin]=useState("")
   const[password, setPassword]=useState("")
   const[email, setEmail]=useState("")
+  const[accounts, setAccounts]=useState([])
+  
+  const handleClick=(e)=>{
+    e.preventDefault()
+    const account={login, password, email}
+    console.log(account)
+    fetch("http://localhost:8080/account/add",{mode: 'no-cors', method:"POST", headers:{"Content-Type":"application/json"}, body:JSON.stringify(account)}).then(()=>{console.log("new Account has been added")})
+  }
+
+  useEffect(()=>{
+    fetch("http://localhost:8080/account/getAll", {mode: 'no-cors'}).then(res=>res.json()).then((result)=>{setAccounts(result);
+})},[])
 
   return (
+    
     <Container>
       <h1>Register</h1>
     <Box
@@ -23,8 +36,13 @@ export default function Account() {
       <TextField id="standard-basic" label="Login" variant="standard"  fullWidth value={login} onChange={(e)=>setLogin(e.target.value)}/>
       <TextField id="standard-basic" label="Password" variant="standard" fullWidth value={password} onChange={(e)=>setPassword(e.target.value)}/>
       <TextField id="standard-basic" label="E-mail" variant="standard" fullWidth value={email} onChange={(e)=>setEmail(e.target.value)}/>
+      
     </Box>
-    <Button variant="contained">Contained</Button>
+    <button variant="outlined" onClick={handleClick}>Register</button>
+    <h1>Accounts</h1>
+    {accounts.map(account=>(<p key={account.id}>Id:{account.id}Login:{account.login}Email:{account.email}</p>))}
     </Container>
+
+    
   );
 }
